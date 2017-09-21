840434759327dd94eef125361e34fd92c522b7cf
luis cordero <info@luiscordero29.com>
2017-09-21 17:46:33 -0400

    Inicial WordPress

---
+++ footer.php
@@ -0,0 +1,8 @@
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/jquery/jquery-3.2.1.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/popper/popper.min.js"></script>
+    <script src="<?php echo get_template_directory_uri(); ?>/assets/bootstrap/js/bootstrap.min.js"></script>
+    <?php wp_footer(); ?>
+</body>
+</html>
---
+++ header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?php wp_title(); ?></title>
+    <?php wp_head(); ?>
+    <!-- Font Awesome -->
+    <link rel="stylesheet"  href="<?php echo get_template_directory_uri(); ?>/assets/font-awesome/css/font-awesome.min.css">
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/assets/bootstrap/css/bootstrap.min.css">
+    <!-- Custom styles for this template -->
+    <link href="<?php echo get_template_directory_uri(); ?>/style.css" rel="stylesheet">
+</head>
+<body>
